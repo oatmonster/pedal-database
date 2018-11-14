20be12494ecc230ad938919838b9e20142bef9a8
Alex Zhao <oatmonster@gmail.com>
2018-11-14 16:40:22 -0500

    Addded register endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 var express = require( 'express' );
 var cors = require( 'cors' );
+var bodyParser = require( 'body-parser' );
 var app = express();
 
 var posts = [
@@ -8,9 +9,16 @@ var posts = [
 ];
 
 app.use( cors() );
+app.use( bodyParser.json() );
 
 app.get( '/posts', ( req, res ) => {
   res.send( posts );
+} );
+
+app.post( '/register', ( req, res ) => {
+  var userData = req.body;
+  res.sendStatus( 200 );
+  console.log( userData );
 } );
 
 app.listen( 3000 );
