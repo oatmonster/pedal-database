63b5dc311dbaf919259a1d72557888688d2383a3
Alex Zhao <oatmonster@gmail.com>
2018-11-16 23:18:56 -0500

    Rearanged file structure

---
+++ client/src/app/routes.ts
@@ -0,0 +1,18 @@
+import { PedalListComponent } from './pedals/pedal-list.component';
+import { PedalDetailsComponent } from './pedals/pedal-details/pedal-details.component';
+import { HomeComponent } from './home/home.component';
+
+export const appRoutes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'pedals',
+    component: PedalListComponent,
+  },
+  {
+    path: 'pedals/:id',
+    component: PedalDetailsComponent,
+  }
+]
