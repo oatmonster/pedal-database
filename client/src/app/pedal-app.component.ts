7179ec2a1d7d454b7a732401394818307a2e0e41
Alex Zhao <oatmonster@gmail.com>
2018-11-19 15:42:11 -0500

    Updated nav bar

--- client/src/app/pedal-app.component.ts
+++ client/src/app/pedal-app.component.ts
@@ -3,6 +3,12 @@ import { Component } from '@angular/core';
 @Component( {
   selector: 'pedal-app',
   templateUrl: './pedal-app.component.html',
+  styles: [ `
+    div.padded {
+      max-width: 80%;
+      margin: auto;
+    }
+  `],
 } )
 export class PedalAppComponent {
   title = 'Pedal Database';
